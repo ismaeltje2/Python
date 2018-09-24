620f74a4f182f5030abc7fed893bdce838988383
ismae <ismael.al-baity@student.hu.nl>
2018-09-24 18:17:22 +0200

    hallo git

---
+++ les 2/x1.py
@@ -0,0 +1,13 @@
+favorieten = ['test']
+
+favorieten.append('test2')
+
+print(favorieten)
+
+favorieten[1] = 'Test3'
+
+print(favorieten)
+--
+lst = [3, 7, -2, 12]
+
+print((max(lst)-(min(lst))
---
+++ les 3/x1.py
@@ -0,0 +1,9 @@
+letters = ('A', 'C', 'B', 'B', 'C', 'A', 'C', 'C', 'B')
+
+a = letters.count('A')
+b = letters.count('B')
+c = letters.count('C')
+
+lst = [a, b, c]
+
+print(lst)
