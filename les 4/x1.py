eb73722254317e70111a9476fd142654e2d7cec9
ismae <ismael.al-baity@student.hu.nl>
2018-09-24 17:42:04 +0200

    les 4

---
+++ les 4/x1.py
@@ -0,0 +1,7 @@
+print(0 ==(1 == 2))
+print((2 + (3 == 4) + 5) == 7)
+print((1 < -1) == (3 > 4))
+--
+uurloon = eval(input('Wat verdien je per uur:'))
+aantal = eval(input('Hoeveel uur heb je gewerkt:'))
+print(aantal, ' uur werken levert ', (aantal * uurloon), 'Euro op')
